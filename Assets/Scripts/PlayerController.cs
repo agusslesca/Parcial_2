8859843437f9aeb21e1879b5f1c10b9993ddb0f7
agusslesca <agustinlescanouni@gmail.com>
2025-06-14 10:18:53 -0300

    Animation: Run and Idle.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,10 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherInput;    // lo hago asi por que los listo con la m para poder usarlos rapido y siempre si son de un mismo game obj
     private Transform m_transform;
+    private Animator m_animator;
     [SerializeField] private float speed;
     private int direction = 1;
+    private int idSpeed;
 
 
     
@@ -17,10 +19,22 @@ public class PlayerController : MonoBehaviour
         m_gatherInput = GetComponent<GatherInput>();
         m_rigidbody2D = GetComponent<Rigidbody2D>();
         m_transform = GetComponent<Transform>();
+        m_animator = GetComponent<Animator>();
+        idSpeed = Animator.StringToHash("Speed"); // convertimos los parametros en numero asi el codigo es mas flexible y no tiene que leer 1 x 1 las letras (optimizacion)
     }
 
-    
-    void Update()
+    private void Update()
+    {
+        SetAnimatorValues();
+        
+    }
+
+    private void SetAnimatorValues()
+    {
+        m_animator.SetFloat(idSpeed, Mathf.Abs(m_rigidbody2D.linearVelocityX)); // abs es una formula matematica para que el numero q pases siempre sea positivo, ya que la conidicon que de idle pase a run es que speed sea mayor q 0
+    }
+
+    void FixedUpdate()
     {
         Move();
     }
