67bfd005ee7b93fd8630105073ec52c54d6671f5
agusslesca <agustinlescanouni@gmail.com>
2025-07-23 17:46:37 -0300

    Randomize diamonds, and animation pickiup

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class Diamond : MonoBehaviour
 {
@@ -6,26 +9,38 @@ public class Diamond : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Rigidbody2D m_rigibody2D;
     [SerializeField] private Animator animator;
+    private int idPickedDiamond;
+    private int idDiamondIndex;
 
     private void Awake()
     {
         m_rigibody2D = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        idPickedDiamond = Animator.StringToHash("pickedDiamond");
+        idDiamondIndex = Animator.StringToHash("diamondIndex");
     }
 
     private void Start()
     {
         gameManager = GameManager.instance;
+        SetRandomDiamond();
+    }
+
+    private void SetRandomDiamond() //Randomizar los colores de los diamonds
+    {
+        var randomDiamondIndex = Random.Range(0, 7);
+        animator.SetFloat(idDiamondIndex, randomDiamondIndex);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            spriteRenderer.enabled = false;
+            //spriteRenderer.enabled = false;
             m_rigibody2D.simulated = false;
             gameManager.AddDiamond();
+            animator.SetTrigger(idPickedDiamond);// ejecutar la animacion de collect del diamond
         }
     }
 }
