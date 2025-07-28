1f8f9b88d341220b97954768ec4d3aaf0b258360
agusslesca <agustinlescanouni@gmail.com>
2025-07-28 19:10:52 -0300

    aggregate button music sound on/off in menuprincipal.

---
+++ Assets/Scripts/ToggleSoundButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleSoundButton : MonoBehaviour
+{
+    public Sprite spriteOff;
+    public Sprite spriteTransition;
+    public Sprite spriteOn;
+    public Image targetImage;
+    public AudioSource musicSource; 
+
+    private bool isSoundOn = true;
+
+    void Start()
+    {
+        // Refleja el estado inicial
+        isSoundOn = musicSource != null && musicSource.volume > 0f;
+        targetImage.sprite = isSoundOn ? spriteOn : spriteOff;
+    }
+
+    public void OnClickToggleSound()
+    {
+        targetImage.sprite = spriteTransition;
+        Invoke(nameof(ApplyToggle), 0.2f);
+    }
+
+    private void ApplyToggle()
+    {
+        isSoundOn = !isSoundOn;
+
+        if (musicSource != null)
+        {
+            musicSource.volume = isSoundOn ? 0.1f : 0f; //  Alterna volumen
+        }
+
+        targetImage.sprite = isSoundOn ? spriteOn : spriteOff;
+    }
+}
+
